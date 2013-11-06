d4d76b49c9d0d70718e3e4ffc2f60ff7c1271886
unlu.yufus <unlu.yufus@gmail.com>
2013-11-06 15:51:45 +0000

    manin page den register ve login işi yapan sayfaya zıplıyor register işlemleri yapıyor db ye login için check ler yapılmadı ajax ve jquery kullanarak hızlıca validation sağlanacak

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Homework 1 yusuf ünlü</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="style_v1.css">
+<!--[if IE]>
+      	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  	<![endif]-->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+<body class="php">
+<div id="container">
+	<div id="main" role="main" class="hellobox">
+		<header><a href="index.php">main page</a></header>
+		<h1><?php echo 'Welcome !'; ?> </h1>
+		</h2>
+	</div>
+	<nav>
+	<ul>
+		<li><a class="active" href="login_register.php">Login</a></li>
+		<li><a href="login_register.php">Register</a></li>
+	</ul>
+	</nav>
+	<footer>
+	<h4>write something</h4>
+	<p>
+		write something:
+	</p>
+	</footer>
+</div>
+</body>
+</html>
