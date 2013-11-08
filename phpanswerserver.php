9555775339b41ab408c4892d1e2eb166b4365468
unlu.yufus <unlu.yufus@gmail.com>
2013-11-08 09:32:33 +0000

    Register panel'i : girilen username db den kontrol edilip onceden kullanılmıssa mesaj basıyor

--- phpanswerserver.php
+++ phpanswerserver.php
@@ -1,9 +1,12 @@
 <?php 
-
-	// pararmetreyi al
+	$connect = mysqli_connect("","root","root","project_v1");
+	if(mysqli_num_rows(mysqli_query($connect,"SELECT * from user WHERE user_name='" . $_GET['data'] . "'")) == 1)
+	{
+		echo $_GET["data"]. " is already in used";
+	}
+	else
+	{
+		echo "OK";
+	}
 	
-	// dbden kontrol et
-	
-	echo "bos bir string yazdir ";
-
 ?>
